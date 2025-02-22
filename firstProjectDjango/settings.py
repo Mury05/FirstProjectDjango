a324b35a93e292752f8519b260104de4a8b85c97
Mury05 <murielleodilone@gmail.com>
2025-02-22 13:13:28 +0100

    Enregistrement de noter app dans notre projet

--- firstProjectDjango/settings.py
+++ firstProjectDjango/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Ajout des nouvelles applications
+    "polls.apps.PollsConfig",
 ]
 
 MIDDLEWARE = [
